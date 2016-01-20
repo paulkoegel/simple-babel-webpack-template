33c76b1f5e69bcd7b16ca8dfa3aba4c2841a3cef
Paul Kögel <paul@railslove.com>
2016-01-20 15:41:48 +0100

    bae.config: shorter regex that does the same.

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -8,7 +8,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         include: path.join(__dirname, '../src'),
         exclude: /node_modules/,
         loader: ['babel'],
